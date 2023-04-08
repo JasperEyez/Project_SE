c8ef333d37df37970e2c7df85ba2e43273356ee9
JasperEyez <119019473+JasperEyez@users.noreply.github.com>
2023-04-08 18:00:34 +0700

    order update

--- client/src/component/OrderMerchandise.js
+++ client/src/component/OrderMerchandise.js
@@ -1,27 +1,33 @@
 import React from 'react'
 import './Order.css'
 import Axios from 'axios'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function OrderMerchandise() {
 
   const [menuList,setMenuList] = useState([]);
 
-  const getMenu = () => {
-    Axios.get('http://localhost:3001/menu').then((response) => {
-      setMenuList(response.data);
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = () => {
+    Axios.get('http://localhost:3001/menu').then((res) => {
+      setMenuList(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  }
+  };
 
   return (
     <div className='py-3'>
-      <button onClick={getMenu}>Show menu</button>
-      <br/>
       <div className="showorder">
           {menuList.map((val, key) => {
               return (
                 <div className="menu card">
                   <div className="card-body">
+                    <Image></Image>
                     <p className="card-text">Name: {val.order_name}</p>
                     <p className="card-text">Details: {val.order_details}</p>
                     <p className="card-text">Price: {val.order_price} ฿</p>
